2a339fec0332d3de9677b9ade33e68b9e3d4632d
trk1234567890 <trk.15.05.2003@gmail.com>
2024-05-06 09:11:21 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,1 +1,14 @@
-print("Hello")
+def linear_search(arr, target):
+
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+
+my_list = [4, 2, 6, 8, 1, 5]
+target_element = 6
+result = linear_search(my_list, target_element)
+if result != -1:
+    print(f"Element {target_element} found at index {result}.")
+else:
+    print(f"Element {target_element} not found.")
